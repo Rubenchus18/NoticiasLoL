306cb353769b4225ce79c0c1a300df0006b49ba4
Rubenchus18 <rubengarci327@gmail.com>
2024-11-11 17:01:22 +0100

    Actualizacion

--- app/src/main/java/com/example/noticiaslol/Noticias.java
+++ app/src/main/java/com/example/noticiaslol/Noticias.java
@@ -1,39 +1,34 @@
 package com.example.noticiaslol;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button botonSiguiente;
+public class Noticias extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_noticias);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        botonSiguiente=findViewById(R.id.LayaoutNoticias);
-        botonSiguiente.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-      Intent e = new Intent(this, Noticias.class);
-       startActivity(e);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
 }
