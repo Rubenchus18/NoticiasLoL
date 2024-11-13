7621e05c096a11837bb267ef56cca6b517557dde
Rubenchus18 <rubengarci327@gmail.com>
2024-11-13 18:41:18 +0100

    Buenas cosas

--- app/src/main/java/com/example/noticiaslol/MainActivity.java
+++ app/src/main/java/com/example/noticiaslol/MainActivity.java
@@ -22,11 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         botonSiguiente=findViewById(R.id.LayaoutNoticias);
         botonSiguiente.setOnClickListener(this);
     }
--- app/src/main/java/com/example/noticiaslol/Noticias.java
+++ app/src/main/java/com/example/noticiaslol/Noticias.java
@@ -1,10 +1,14 @@
 package com.example.noticiaslol;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,13 +19,8 @@ public class Noticias extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_noticias);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 
     @Override
@@ -29,6 +28,25 @@ public class Noticias extends AppCompatActivity {
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Valorant:
+                Intent intentValorant = new Intent(this, Valorant.class);
+                startActivity(intentValorant);
+                break;
+            case R.id.Lol:
+                Intent intentLOL = new Intent(this, LOL.class);
+                startActivity(intentLOL);
+                break;
+            case R.id.CSGO:
+                Intent intentcsgo = new Intent(this, CsGo.class);
+                startActivity(intentcsgo);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
